7e4b2df422b2764884b536f1214c101b1e9c3958
vladislav-og <vladislav.ogorodnik@mail.ru>
2022-12-02 22:43:05 +0200

    AoC 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,43 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var caloriesCounter = 0
+        for (row in input) {
+            if (row == "") {
+                if (maxCalories < caloriesCounter) {
+                    maxCalories = caloriesCounter
+                }
+                caloriesCounter = 0
+                continue
+            }
+            caloriesCounter += row.toInt()
+        }
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val topThreeValues = arrayListOf(0, 0, 0)
+        var caloriesCounter = 0
+        for (row in input) {
+            if (row == "") {
+                if (caloriesCounter > topThreeValues[0]) {
+                    topThreeValues[0] = caloriesCounter
+                    topThreeValues.sort()
+                }
+                caloriesCounter = 0
+                continue
+            }
+            caloriesCounter += row.toInt()
+        }
+        return topThreeValues.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    println(part1(testInput))
+    println(part2(testInput))
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
